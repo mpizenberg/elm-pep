10102501c7463c27c3117b818195c0b66918b401
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2017-12-11 21:34:51 +0800

    Use variable instead of adding dom attributes

--- elm-pep.js
+++ elm-pep.js
@@ -2,6 +2,11 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
+// Variable to hold current primary touch event identifier.
+// iOS needs this since it does not attribute
+// identifier 0 to primary touch event.
+let primaryTouchId = null;
+
 if (!("PointerEvent" in window)) {
   // Create Pointer polyfill from mouse events only on non-touch device
   if (!("TouchEvent" in window)) {
@@ -57,15 +62,14 @@ if (!("PointerEvent" in window)) {
           pointerEvent.pointerId = 1 + touch.identifier;
 
           // First touch is the primary pointer event.
-          if (touchType === "touchstart" && !elmPepTarget.hasPrimaryTouchId) {
-            elmPepTarget.hasPrimaryTouchId = true;
-            elmPepTarget.primaryTouchId = touch.identifier;
+          if (touchType === "touchstart" && primaryTouchId === null) {
+            primaryTouchId = touch.identifier;
           }
-          pointerEvent.isPrimary =
-            touch.identifier === elmPepTarget.primaryTouchId;
+
+          // If first touch ends, reset primary touch id.
+          pointerEvent.isPrimary = touch.identifier === primaryTouchId;
           if (touchType === "touchend" && pointerEvent.isPrimary) {
-            elmPepTarget.hasPrimaryTouchId = false;
-            elmPepTarget.primaryTouchId = null;
+            primaryTouchId = null;
           }
 
           elmPepTarget.dispatchEvent(pointerEvent);
